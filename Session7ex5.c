a699e36aa87ef6e9de235d3d024fe33b981ff452
hluong2702 <hoangluong2702@gmail.com>
2024-11-28 14:05:00 +0700

    ss7

--- Session7ex5.c
+++ Session7ex5.c
@@ -4,12 +4,17 @@ int main(){
 
     int numbers[5]={1,2,3,8,9};
     int max = numbers[0];
+    int min = numbers[0];
     for(int i = 0;i<5;i++){
        if(numbers[i]> max){
         max = numbers[i];
     }
-}
-    printf("Max %d",max);
+       if(numbers[i]< min){
+        min = numbers[i];
+       }
 
+}
+    printf("Max %d\n",max);
+    printf("Min %d\n",min);
     return 0;
 }    
